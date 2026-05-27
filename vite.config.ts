bf08ae1e6e3184ae05fc09257ddb9dcb3aedc7b6
Blake Weichbrodt <blakeweichbrodt7@gmail.com>
2026-05-26 23:29:27 -0500

    Updated Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/blakeweichbrodt.io/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 
 })
