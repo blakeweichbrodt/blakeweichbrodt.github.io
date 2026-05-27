a3e5778e7f3b95e88841d5eec0f4ffec2b5b94fd
Blake Weichbrodt <blakeweichbrodt7@gmail.com>
2026-05-27 18:20:21 -0500

    Added some temporary components to website

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,19 @@
 // pages/Projects.tsx
+import {Map} from '@vis.gl/react-maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css'; // See notes below
+
 export default function Projects() {
   return (
-    <div className="pt-24 text-white">
-      <h1 className="text-4xl font-bold">Projects</h1>
+    <div className="relative flex min-h-dvh w-full items-center justify-center overflow-hidden bg-black px-6">
+      <Map
+        initialViewState={{
+          longitude: -100,
+          latitude: 40,
+          zoom: 3.5
+        }}
+        style={{width: 1000, height: 600}}
+        mapStyle="https://demotiles.maplibre.org/style.json"
+      />
     </div>
   );
 }
